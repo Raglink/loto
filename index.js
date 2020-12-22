e0292401f74d7c2c4527e4acd4e50f4297abc03a
Vincent Fagoaga <fago40@gmail.com>
2020-12-22 18:10:52 +0100

    get unique values

--- index.js
+++ index.js
@@ -1,35 +1,66 @@
 const listOfLotoGrids = (numberOfGrids) =>{
-    let allLotoGrids = ""
+    // All constants
     const countNumber = 5
     const minNumber =1
     const maxNumber =49
-
+    
     const countStar = 1
-    const maxStar =1
-    const minStar =10
-
+    const minStar =1
+    const maxStar =10
+    
+    // Functions
     const getNumber = (min, max) => {
         limitMin = Math.ceil(min)
         limitMax = Math.floor(max)
         return Math.floor(Math.random() * (limitMax - limitMin +1)+ limitMin)
     }
 
-    for (let i = 0; i< numberOfGrids; i++){
-        // get number
-        for(let j = 0 ;j<countNumber;j++){
-            allLotoGrids += getNumber(minNumber,maxNumber)
-            if(j <countNumber - 1){
-                allLotoGrids += " , "
+    const addValueToArray = (arrayToCheck, min , max) =>{
+        let isFound = false
+        while (!isFound){
+            let numberToCheck = getNumber(min,max)
+            if(arrayToCheck.indexOf(numberToCheck)== -1){
+            isFound = true
+            arrayToCheck.push(numberToCheck)
             }
         }
-        // get stat 
-        for (let j =0; j<countStar; j++){
-            allLotoGrids +=` et ${getNumber(minStar,maxStar)} \n`
-
+    }
+    const displayValues = (arraySource)=>{
+        let valueToDisplay = ""
+        for(let i =0 ; i<arraySource.length ;i++ ){
+            valueToDisplay+=arraySource[i]
+            if(i<arraySource.length-1){
+                valueToDisplay+=","
+            }
         }
-
+        return valueToDisplay
+    }
+    const createAGrid = (grid, gridSize, min, max) => {
+        for(let i = 0 ;i<gridSize;i++){
+            addValueToArray(grid, min, max)
+        }
+        grid.sort(function(a, b){return a - b})
+    }
+    
+    // Create Grids
+    let allLotoGrids = ""
+ 
+    for (let i = 0; i< numberOfGrids; i++){
+        // get number
+        const numberOfTheGrid = []
+        createAGrid(numberOfTheGrid, countNumber, minNumber, maxNumber)
+        
+        // get stat 
+        const starOfTheGrid = []
+        createAGrid(starOfTheGrid,countStar, minStar, maxStar)
+        
+        // display values
+        allLotoGrids+= displayValues(numberOfTheGrid)
+        allLotoGrids+= " et "
+        allLotoGrids+= displayValues(starOfTheGrid)
+        allLotoGrids+="\n"
     }
 
     return allLotoGrids
 }
-console.log(listOfLotoGrids(4))
+console.log(listOfLotoGrids(20))
